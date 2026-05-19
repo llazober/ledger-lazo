fe009ef47f628547a3aab0c03093b2ff753ea320
Deployment Agent <luislazober@gmail.com>
2026-05-18 23:40:16 -0400

    fix: implement static index.html root health check bypass

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   basePath: '/accounting',
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/accounting',
-        permanent: true,
-        basePath: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
